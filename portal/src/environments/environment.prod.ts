a8b9cec64f7313c0f22ea9df92e5fcf6d0b33430
John Scharber <john@pavedroad.io>
2021-08-31 06:52:07 -1000

    Change Auth0 URLs to eventorchestrator.us.auth0.com

--- portal/src/environments/environment.prod.ts
+++ portal/src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
   CustBaseURL: "http://api.pavedroad.io",
   OrgBaseURL: "http://api.pavedroad.io",
-  BaseURL: "api.pavedroad.io"
-  BasePath: "/api/v1/namespace/pavedroad/"
-  OrgEndPoint: "organization" 
-  EBConfigEndPoint: "customers" 
-  ManifestEndPoint: "EoManifestMgr" 
+  BaseURL: "api.pavedroad.io",
+  BasePath: "/api/v1/namespace/pavedroad/",
+  OrgEndPoint: "organization",
+  EBConfigEndPoint: "customers" ,
+  ManifestEndPoint: "EoManifestMgr",
 
   // Auth0
-  Domain: "pavedroad.us.auth0.com",
-  Audience: "https://pavedroad.us.auth0.com/api/v2/",
+  Domain: "eventorchestrator.us.auth0.com",
+  Audience: "https://eventorchestrator.us.auth0.com/api/v2/",
   ClientID: "8R24SiFyQn8ECiWBH8VEGwBS2GfaQnXK",
   Scope: "read:current_user"
 

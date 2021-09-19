7024b789c2b09355e61dd9078c3531e6dbcac41d
John Scharber <john@pavedroad.io>
2021-09-18 16:38:41 -1000

    WIP for hooking deployments with eo-manifest-mgr

--- portal/src/app/pages/deploy/deploment/deploment.component.ts
+++ portal/src/app/pages/deploy/deploment/deploment.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { DeploymentService } from '../../../services/deployment.service';
+
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
 
 @Component({
   selector: 'app-deploment',
@@ -9,11 +14,29 @@ export class DeplomentComponent implements OnInit {
   manifestProgress: number;
   deploymentProgress: number;
   schedulerProgress: number;
+  status: string;
 
-  constructor() {}
+  constructor(private deploymentSvc: DeploymentService) {}
 
   ngOnInit(): void {
     this.generateManifests();
+  }
+
+  ngAfterViewInit(): void {
+    this.loadStatus();
+  }
+
+  loadStatus() {
+    this.deploymentSvc.getDeployment().subscribe((data: string) => {
+      this.status = data;
+      console.log("Data: ", data);
+      if ( data.includes('Profile not loaded') ) {
+        console.log('DATA', data);
+        sleep(1000).then(() => {
+		this.loadStatus();
+	});
+      }
+    });
   }
 
   generateManifests() {
@@ -29,5 +52,4 @@ export class DeplomentComponent implements OnInit {
   updateScheuler() {
     this.schedulerProgress = 25;
   }
-
 }
